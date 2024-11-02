d22f44bd083b25c68097d2576a96390d95f55c82
Spencer Norman <spencerdnorman@gmail.com>
2024-11-02 17:27:41 -0500

    commented out the autogenerated systems test for now, not ready for it

--- test/system/tournaments_test.rb
+++ test/system/tournaments_test.rb
@@ -1,45 +1,45 @@
 require "application_system_test_case"
 
 class TournamentsTest < ApplicationSystemTestCase
-  setup do
-    @tournament = tournaments(:one)
-  end
+  # setup do
+  #   @tournament = tournaments(:one)
+  # end
 
-  test "visiting the index" do
-    visit tournaments_url
-    assert_selector "h1", text: "Tournaments"
-  end
+  # test "visiting the index" do
+  #   visit tournaments_url
+  #   assert_selector "h1", text: "Tournaments"
+  # end
 
-  test "should create tournament" do
-    visit tournaments_url
-    click_on "New tournament"
+  # test "should create tournament" do
+  #   visit tournaments_url
+  #   click_on "New tournament"
 
-    fill_in "Account", with: @tournament.account_id
-    fill_in "Name", with: @tournament.name
-    fill_in "Organization", with: @tournament.organization_id
-    click_on "Create Tournament"
+  #   fill_in "Account", with: @tournament.account_id
+  #   fill_in "Name", with: @tournament.name
+  #   fill_in "Organization", with: @tournament.organization_id
+  #   click_on "Create Tournament"
 
-    assert_text "Tournament was successfully created"
-    click_on "Back"
-  end
+  #   assert_text "Tournament was successfully created"
+  #   click_on "Back"
+  # end
 
-  test "should update Tournament" do
-    visit tournament_url(@tournament)
-    click_on "Edit this tournament", match: :first
+  # test "should update Tournament" do
+  #   visit tournament_url(@tournament)
+  #   click_on "Edit this tournament", match: :first
 
-    fill_in "Account", with: @tournament.account_id
-    fill_in "Name", with: @tournament.name
-    fill_in "Organization", with: @tournament.organization_id
-    click_on "Update Tournament"
+  #   fill_in "Account", with: @tournament.account_id
+  #   fill_in "Name", with: @tournament.name
+  #   fill_in "Organization", with: @tournament.organization_id
+  #   click_on "Update Tournament"
 
-    assert_text "Tournament was successfully updated"
-    click_on "Back"
-  end
+  #   assert_text "Tournament was successfully updated"
+  #   click_on "Back"
+  # end
 
-  test "should destroy Tournament" do
-    visit tournament_url(@tournament)
-    click_on "Destroy this tournament", match: :first
+  # test "should destroy Tournament" do
+  #   visit tournament_url(@tournament)
+  #   click_on "Destroy this tournament", match: :first
 
-    assert_text "Tournament was successfully destroyed"
-  end
+  #   assert_text "Tournament was successfully destroyed"
+  # end
 end
